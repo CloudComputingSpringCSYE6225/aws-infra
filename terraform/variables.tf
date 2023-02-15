fb7a04d281f07307ef7f97f882f8b7ea95650365
Rebecca Biju <biju.r@northeastern.edu>
2023-02-14 20:56:04 -0500

    Initial infra

---
+++ terraform/variables.tf
@@ -0,0 +1,43 @@
+variable "region" {
+  type        = string
+  description = "AWS region to use"
+  default = "us-east-1"
+}
+
+variable "profile" {
+  type        = string
+  description = "Profile to use for deployment"
+  default = "dev"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "VPC CIDR block"
+  default = "10.0.0.0/16"
+}
+
+variable "public_cidr_block" {
+  type        = string
+  description = "VPC CIDR block"
+  default = "0.0.0.0/0"
+}
+
+variable "public_subnet_count" {
+  type = number
+  default = 3
+}
+
+variable "private_subnet_count" {
+  type = number
+  default = 3
+}
+
+#variable "public_subnets" {
+#  type = list
+#  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#}
+#
+#variable "private_subnets" {
+#  type = list
+#  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+#}
---
+++ terraform/vpc.tf
@@ -0,0 +1,65 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+#Use Data Source to get all availability zones in the region
+data "aws_availability_zones" "zones" {
+  state = "available"
+}
+
+#Create Public Subnets
+resource "aws_subnet" "public_subnets" {
+  count                   = var.public_subnet_count
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr_block, 8, count.index+1)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.zones.names[count.index]
+  tags = {
+    Name ="Public Subnet-${count.index}"
+  }
+}
+
+#Create Public Route Table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = var.public_cidr_block
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+#Associate Public Subnets to Route Table
+resource "aws_route_table_association" "public_route_table_association" {
+  count          = var.public_subnet_count
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+#Create Private Subnets
+resource "aws_subnet" "private_subnets" {
+  count                   = var.private_subnet_count
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr_block, 8, count.index+var.public_subnet_count+1)
+  map_public_ip_on_launch = false
+  availability_zone       = data.aws_availability_zones.zones.names[count.index]
+  tags = {
+    Name ="Private Subnet-${count.index}"
+  }
+}
+
+#Create Private Route Table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+#Associate Private Subnets to Route Table
+resource "aws_route_table_association" "private_route_table_association" {
+  count          = var.private_subnet_count
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
