878090a131d23156bcdf68c48f20a5c5d34d8244
Rebecca Biju <biju.r@northeastern.edu>
2023-03-01 22:01:54 -0500

    S3 files

---
+++ terraform/database.tf
@@ -0,0 +1,32 @@
+resource "aws_db_parameter_group" "pg_parameter_group" {
+  name        = "pg-14-parameter-group"
+  family      = "postgres14"
+  description = "Parameter Group for PostgreSQL 14"
+}
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "main"
+  subnet_ids = [for subnet in aws_subnet.private_subnets : subnet.id]
+
+  tags = {
+    Name = "DB subnet group"
+  }
+}
+
+resource "aws_db_instance" "main" {
+
+  engine                 = var.db_engine
+  instance_class         = var.db_instance
+  identifier             = var.db_instance_identifier
+  db_name                = var.db_name
+  username               = var.db_username
+  multi_az               = var.db_multi_az
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
+  parameter_group_name   = aws_db_parameter_group.pg_parameter_group.name
+  publicly_accessible    = false
+  allocated_storage      = 10
+  vpc_security_group_ids = [aws_security_group.db_security_group.id]
+  skip_final_snapshot    = true
+
+}
---
+++ terraform/roles.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "EC2-CSYE6225" {
+  name = "EC2-CSYE6225"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "WebAppS3_policy_attachment" {
+  policy_arn = aws_iam_policy.WebAppS3.arn
+  role = aws_iam_role.EC2-CSYE6225.name
+}
