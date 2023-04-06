d4b7eaa6f1388b56385a9ea9c4c5bc8d41804fcb
Rebecca Biju <biju.r@northeastern.edu>
2023-04-06 12:19:40 -0400

    Remove key name and disable api termination

--- terraform/launchTemplate.tf
+++ terraform/launchTemplate.tf
@@ -10,7 +10,7 @@ resource "aws_launch_template" "asg_launch_template" {
   name          = "asg_launch_config"
   image_id      = data.aws_ami.custom_ami.id
   instance_type = var.instance_type
-  key_name      = var.key_name
+#  key_name      = var.key_name
   user_data     = base64encode(data.template_file.userData.rendered)
   network_interfaces {
     associate_public_ip_address = true
@@ -19,7 +19,7 @@ resource "aws_launch_template" "asg_launch_template" {
   iam_instance_profile {
     name = aws_iam_instance_profile.EC2-CSYE6225_instance_profile.name
   }
-  disable_api_termination = true
+  disable_api_termination = false
   block_device_mappings {
     device_name = "/dev/xvda"
     ebs {
